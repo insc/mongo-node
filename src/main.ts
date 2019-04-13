55b1066f0d8caf43b5a259eede573a1be981a250
ingo <ingo.schnabel@gmail.com>
2019-04-13 15:38:48 +0200

    Init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import http from "http";
+
+const app = express();
+
+let helloWorld : String = "Hello world!!";
+let halloWelt : String = "Hallo Welt!!!";
+
+console.log('Server started!');
